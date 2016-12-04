f43fb58a0ff96711600ce660469ef6aa030801a1
Aleksey <huker28@gmail.com>
2016-12-04 22:32:59 +0300

    Added alert extension. (Alerts are using the Bootsrap alerts)

---
+++ MVC5Extensions/MVC5Extensions/Alerts/Alert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC5Extensions.Alerts
+{
+    public class Alert
+    {
+        public string AlertClass { get; set; }
+
+        public string Message { get; set; }
+
+        public Alert(string alertClass, string message)
+        {
+            this.AlertClass = alertClass;
+            this.Message = message;
+        }
+    }
+}
---
+++ MVC5Extensions/MVC5Extensions/Alerts/AlertDecoratorResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC5Extensions.Alerts
+{
+    public class AlertDecoratorResult : ActionResult
+    {
+        public ActionResult InnerResult { get; set; }
+
+        public string AlertClass { get; set; }
+
+        public string Message { get; set; }
+
+        public AlertDecoratorResult(ActionResult innerResult, string alertClass, string message)
+        {
+            this.InnerResult = innerResult;
+            this.AlertClass = alertClass;
+            this.Message = message;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var alerts = context.Controller.TempData.GetAlerts();
+            alerts.Add(new Alert(this.AlertClass, this.Message));
+            this.InnerResult.ExecuteResult(context);
+        }
+    }
+}
